a2394adcc0c2553fd103d7c0173113db78aeb98d
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2025-06-12 19:44:35 +0200

    feat: Auto detect versions and add modVersion process resource

--- packetfixer/build.gradle.kts
+++ packetfixer/build.gradle.kts
@@ -9,21 +9,30 @@ plugins {
 
 version = rootProject.version
 
-java.toolchain.languageVersion.set(JavaLanguageVersion.of(8))
-
-dependencies {
-}
-
 tasks.register<Jar>("mergedJar") {
     archiveClassifier.set("merged")
 
-    dependsOn("jar")
-    dependsOn(":fabric:v1_16_5:remapJar", ":forge:v1_16_5:jar")
+    dependsOn("jar", ":common:jar")
 
     val resourcesJar = tasks.named<Jar>("jar").get().archiveFile.get().asFile
     val commonJar = project(":common").tasks.named<Jar>("jar").get().archiveFile.get().asFile
-    val fabricJar = project(":fabric:v1_16_5").tasks.named<RemapJarTask>("remapJar").get().archiveFile.get().asFile
-    val forgeJar = project(":forge:v1_16_5").tasks.named<Jar>("jar").get().archiveFile.get().asFile
+
+    val loadersJars = mutableListOf<File>()
+
+    project(":fabric").subprojects.forEach { project ->
+        dependsOn(":fabric:${project.name}:remapJar")
+        val fabricJar = project(":fabric:${project.name}").tasks.named<RemapJarTask>("remapJar").get().archiveFile.get().asFile
+        loadersJars.add(fabricJar)
+    }
+
+    val forgeMixins = mutableListOf<String>()
+    
+    project(":forge").subprojects.forEach { project ->
+        dependsOn(":forge:${project.name}:jar")
+        val forgeJar = project(":forge:${project.name}").tasks.named<Jar>("jar").get().archiveFile.get().asFile
+        loadersJars.add(forgeJar)
+        forgeMixins.add("packetfixer.${project.name}.forge.mixins.json")
+    }
 
     manifest {
         attributes(
@@ -34,14 +43,31 @@ tasks.register<Jar>("mergedJar") {
             "Implementation-Version" to version,
             "Implementation-Vendor" to "TonimatasDEV",
             "Implementation-Timestamp" to DateTimeFormatter.ISO_INSTANT.format(Instant.now()),
-            "MixinConfigs" to "packetfixer.v1_16_5.forge.mixins.json"
+            "MixinConfigs" to forgeMixins.joinToString(","),
         )
     }
-    
+
     from(zipTree(resourcesJar))
     from(zipTree(commonJar))
-    from(zipTree(fabricJar))
-    from(zipTree(forgeJar))
+
+    loadersJars.forEach { file ->
+        from(zipTree(file))
+    }
 
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
+tasks.processResources {
+    val commonProperties = mapOf("modVersion" to rootProject.version)
+
+    inputs.properties(commonProperties)
+
+    filesMatching(listOf("META-INF/mods.toml", "fabric.mod.json")) {
+        expand(commonProperties)
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
